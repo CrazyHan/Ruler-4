cd3d0a100960533f2410394f1f3012ede5c4c719
guanguoxiang <abcgg8@qq.com>
2016-03-06 00:49:19 +0800

    版本更新

--- app/src/main/java/com/ggx/ruler/MainActivity.java
+++ app/src/main/java/com/ggx/ruler/MainActivity.java
@@ -2,21 +2,28 @@ package com.ggx.ruler;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
 import android.widget.TextView;
+
+import com.ggx.ruler_lib.RulerView;
 
 public class MainActivity extends AppCompatActivity implements RulerView.RulerCallback{
 
     TextView tv;
     RulerView rv;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         tv= (TextView) findViewById(R.id.tv);
         rv= (RulerView) findViewById(R.id.rv);
-        rv.setCallback(this);
+        //rv.setCallback(this);
+        rv.setCallback(new RulerView.RulerCallback() {
+            @Override
+            public void resultNum(int num) {
+                tv.setText("身高"+num+"cm");
+            }
+        });
     }
 
     @Override
